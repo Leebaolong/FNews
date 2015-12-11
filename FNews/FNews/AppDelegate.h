8c916174cb15831d18268d8d482d226c7aebb082
Leebaolong <Leebaolong998@yeah.net>
2015-12-11 14:45:40 +0800

    1.初建项目

---
+++ FNews/FNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FNews
+//
+//  Created by apple on 15/12/11.
+//  Copyright © 2015年 CoderLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
